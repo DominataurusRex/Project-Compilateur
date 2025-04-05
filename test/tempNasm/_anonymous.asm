2a4be04511f98e9ccfc6fc706dedc6896139bad3
DominataurusRex <95956715+DominataurusRex@users.noreply.github.com>
2025-04-05 19:11:00 +0200

    Ajour fonction
    Ajour de putchar/int et getchar

--- test/tempNasm/_anonymous.asm
+++ test/tempNasm/_anonymous.asm
@@ -4,8 +4,11 @@ section .data
 
 ;-------------- Variable Global --------------;
 section .bss
-    global_var resb 8       ; Reserve 8x 1 octet
+    GLOBAL_VAR resb 8       ; Reserve 8x 1 octet
 ;-------------- Variable Global --------------;
+    CHAR_BUFF resb 1
+    INT_BUFF resb 16
+
 section .text
     global _start
 
@@ -29,6 +32,61 @@ show_registers:
     ret
 
 
+putchar:                       ; Parametre rdi
+    mov [CHAR_BUFF], rdi
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, CHAR_BUFF
+    mov rdx, 1
+    syscall
+    ret
+
+
+putint:                         ; Parametre rdi
+    mov r11, INT_BUFF + 16
+    mov rax, rdi
+    mov rbx, 10
+    cmp rdi, 0
+
+    jpe .convert_putint         ; Le rend positif
+    neg rax
+
+    .convert_putint:            ; Boucle de conversion
+        xor rdx, rdx            ; Reset rdx
+        div rbx                 ; rax = /10 // rdx = %10
+        add rdx, '0'            ; conversion
+        dec r11                 ; Decalage du pointeur du buffer
+        mov [r11], dl           ; Deplacmement dans le buffer
+        test rax, rax
+        jnz .convert_putint
+
+    cmp rdi, 0                  ; Rajoute le `-` si negatif
+    jpe .end_putint
+    mov r10, '-'
+    dec r11
+    mov [r11], r10b
+
+    .end_putint:                ; Affiche
+        mov rax, 1
+        mov rdi, 1
+        mov rsi, r11
+        mov rdx, INT_BUFF + 16
+        sub rdx, r11
+        syscall
+        ret
+
+
+getchar:                        ; Retour rax
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, CHAR_BUFF
+    mov rdx, 1
+    syscall
+    xor rax, rax
+    mov al, [CHAR_BUFF]
+    ret
+
+
 test_funct:
     ;-------------- Variables Local --------------;
     push rbp                ; Sauvegarde ancien pointeur de base
@@ -39,12 +97,13 @@ test_funct:
     sub rsp, 8                      ; 8 Octets (ex: char / int)
 
     ; -- Affectation --
-    mov dword [rbp - 4], 'a'        ; Adresse relative : 4
-    mov dword [rbp - 8], 34         ; Adresse relative : 8
+    mov byte [rbp - 1], 'a'        ; Adresse relative : 4
+    mov dword [rbp - 5], 34         ; Adresse relative : 8
 
     ; Recuperation   --
-    mov ebx, [rbp - 4]
-    mov r12d, [rbp - 8]
+    xor rbx, rbx
+    mov bl, [rbp - 1]
+    mov r12d, [rbp - 5]
 
     ;-----------------------;
     mov rsp, rbp            ; Replace le sommet de pile a la base de la fonction
@@ -68,12 +127,13 @@ _start:
     ;-------------- Alignement Pile --------------;
 
     ;---- Ex var global ----;
-    mov dword [global_var], 76      ; Adresse relative : 0
-    mov dword [global_var + 4], 138 ; Adresse relative : 4
+    mov dword [GLOBAL_VAR], 76      ; Adresse relative : 0
+    mov dword [GLOBAL_VAR + 4], 138 ; Adresse relative : 4
 
-    mov r13d, [global_var]
-    mov r14d, [global_var + 4]
+    mov r13d, [GLOBAL_VAR]
+    mov r14d, [GLOBAL_VAR + 4]
     ;---- Ex var global ----;
+
 
     call show_registers
 
